10e5644ea0c5a1d6292237266e9a8ec7446b20bb
Michael Zou <michaelzyzou@gmail.com>
2024-02-03 16:16:12 -0500

    Did the shooter PID

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,49 +6,97 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
+import com.revrobotics.SparkRelativeEncoder.Type;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.MotorPorts;
 import frc.robot.Constants.PneumaticPorts;
 
 public class Shooter extends SubsystemBase {
+
+  private final CANSparkMax shooterMotor = new CANSparkMax(MotorPorts.motorShooter, MotorType.kBrushless);
+  private final DoubleSolenoid flap = new DoubleSolenoid(PneumaticsModuleType.REVPH, PneumaticPorts.flapForward,
+      PneumaticPorts.flapReverse);
+  private final RelativeEncoder shooterEncoder = shooterMotor.getEncoder(Type.kHallSensor, 42);
+  private SparkPIDController shooterPid = shooterMotor.getPIDController();
+  private double lowReference = 200;
+  private double highReference = 600;
+  private boolean shootHigh;
+  // PID coefficients
+  private final double kP = 6e-5;
+  private final double kI = 0;
+  private final double kD = 0;
+  private final double kIz = 0;
+  private final double kFF = 0.000015;
+  private final double kMaxOutput = 1;
+  private final double kMinOutput = -1;
+  private final double maxRPM = 5700;
+
   /** Creates a new Shooter. */
-  public Shooter() {}
-  private final CANSparkMax shooterMotor = new CANSparkMax(MotorPorts.motorShooter, MotorType.kBrushless); 
-  private final DoubleSolenoid flap = new DoubleSolenoid(PneumaticsModuleType.REVPH, PneumaticPorts.flapForward, PneumaticPorts.flapReverse);
- 
+  public Shooter() {
+    // set PID coefficients
+    shooterPid.setP(kP);
+    shooterPid.setI(kI);
+    shooterPid.setD(kD);
+    shooterPid.setIZone(kIz);
+    shooterPid.setFF(kFF);
+    shooterPid.setOutputRange(kMinOutput, kMaxOutput);
+    
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
   }
+
   /**
    * sets motor and flap for lowshot
    */
-  public void lowShot(){
-    shooterMotor.set(0.5);
+  public void lowShot() {
     flap.set(DoubleSolenoid.Value.kForward);
+    shooterPid.setReference(lowReference, CANSparkMax.ControlType.kSmartVelocity);
+    shootHigh = false;
   }
+
   /**
    * sets motor and flap for highshot
    */
-  public void highShot(){
-    shooterMotor.set(1);
+  public void highShot() {
+    shooterPid.setReference(highReference, CANSparkMax.ControlType.kSmartVelocity);
     flap.set(DoubleSolenoid.Value.kReverse);
+    shootHigh = true;
   }
 
   /**
    * stops the index motor
    */
-  public void stop(){
+  public void stop() {
     shooterMotor.set(0);
     flap.set(DoubleSolenoid.Value.kReverse);
   }
+
   /**
    * Returns true if at right speed
    */
-  public boolean atSpeed(){
-    return true;
+  public boolean atSpeed() {
+    if (shootHigh){
+      return (highReference - 20 < shooterEncoder.getVelocity() && shooterEncoder.getVelocity() < highReference + 20);
+    } else {
+      return (lowReference - 20 < shooterEncoder.getVelocity() && shooterEncoder.getVelocity() < lowReference + 20);
+    }
   }
+
 }

81c93dc7d84e9d13d94c562d0dc43f88e8f04950
Sean <sean@ertw.com>
2024-02-24 13:44:19 -0500

    Use the pid control for shooter
    I think the problem was we used the wrong encoder type. Going with
    defaults per the instructions.
    
    Also crank up the speeds a bit and display to smartdashboard

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -46,8 +46,7 @@ public class SwerveSubsystem extends SubsystemBase {
   /**
    * Maximum speed of the robot in meters per second, used to limit acceleration.
    */
-  // public double maximumSpeed = Units.feetToMeters(14.5);
-  public double maximumSpeed = 3;
+  public double maximumSpeed = 4.5;
 
   /**
    * Initialize {@link SwerveDrive} with the directory provided.
